8fe18eee7d81ae3d808611f2a784239ae77a0b39
Victor Mendoza <victor@lucaszander.com>
2026-04-19 14:03:25 -0500

    update tf

--- devops/terraform/providers.tf
+++ devops/terraform/providers.tf
@@ -5,3 +5,13 @@ provider "aws" {
     tags = local.default_tags
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-management-824398329482348"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "tfstate"
+  }
+}
