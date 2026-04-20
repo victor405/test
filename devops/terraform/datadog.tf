5002ae6cf0693a34e1bb353591b7c3faf463fa65
Victor Mendoza <victor@lucaszander.com>
2026-04-19 22:48:06 -0500

    test

--- devops/terraform/datadog.tf
+++ devops/terraform/datadog.tf
@@ -6,7 +6,7 @@ resource "datadog_synthetics_test" "cloudfront_uptime" {
 
   request_definition {
     method = "GET"
-    url    = "https://d2dk4o2rttwzws.cloudfront.net"
+    url    = "https://d2dk4o2rttwzws.cloudfront.net/health"
   }
 
   assertion {
@@ -27,37 +27,5 @@ resource "datadog_synthetics_test" "cloudfront_uptime" {
   }
 
   message = "CloudFront DOWN"
-  tags    = ["env:dev", "service:frontend"]
-}
-
-resource "datadog_synthetics_test" "github_pages_uptime" {
-  name    = "github-pages uptime"
-  type    = "api"
-  subtype = "http"
-  status  = "live"
-
-  request_definition {
-    method = "GET"
-    url    = "https://victor405.github.io/test"
-  }
-
-  assertion {
-    type     = "statusCode"
-    operator = "is"
-    target   = 200
-  }
-
-  locations = ["aws:us-east-1"]
-
-  options_list {
-    tick_every = 60
-
-    retry {
-      count    = 2
-      interval = 3000
-    }
-  }
-
-  message = "GitHub Pages DOWN"
   tags    = ["env:dev", "service:frontend"]
 }
