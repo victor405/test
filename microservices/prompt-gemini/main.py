3237183c0155013e76eea34e14ab445298d1da2c
Victor Mendoza <victor@lucaszander.com>
2026-04-19 20:26:18 -0500

    update cors

--- microservices/prompt-gemini/main.py
+++ microservices/prompt-gemini/main.py
@@ -4,8 +4,16 @@ import boto3
 import pymysql
 import requests
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ---------- DB ----------
 def get_db():
